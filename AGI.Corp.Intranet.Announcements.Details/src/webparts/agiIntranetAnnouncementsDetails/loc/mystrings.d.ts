398a1b164c96db301707156f3fb0b3e66b9dd557
Ranjith Muthukumar <Ranjith.muthukumar@techcarrot.ae>
2022-08-22 15:31:12 +0400

    initial commit for announcement details webpart

---
+++ AGI.Corp.Intranet.Announcements.Details/src/webparts/agiIntranetAnnouncementsDetails/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAgiIntranetAnnouncementsDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AgiIntranetAnnouncementsDetailsWebPartStrings' {
+  const strings: IAgiIntranetAnnouncementsDetailsWebPartStrings;
+  export = strings;
+}
