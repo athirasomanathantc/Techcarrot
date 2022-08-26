aea7edf519235957960ea27a43f5648fe80069f2
GokulaSivaRaja <gokula.sivaraja@techcarrot.ae>
2022-08-26 12:01:45 +0400

    theme solution

---
+++ AGI.Corp.Intranet.Theme/src/extensions/agiIntranetTheme/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAgiIntranetThemeApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AgiIntranetThemeApplicationCustomizerStrings' {
+  const strings: IAgiIntranetThemeApplicationCustomizerStrings;
+  export = strings;
+}
