ad65185c84e15bce62087225abb93142afd7d1e7
Mohammed Jabir <mohammed.jabir@techcarrot.ae>
2022-09-05 15:27:35 +0400

    calendar control in home page

--- AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/Calendar/Calendar.tsx
+++ AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/Calendar/Calendar.tsx
@@ -1,23 +1,30 @@
 import * as React from "react";
-import { IAgiIntranetHomeMainProps } from "../IAgiIntranetHomeMainProps";
+import { useState } from "react";
+import DayPicker from "react-day-picker";
+import 'react-day-picker/lib/style.css';
 
-export const Calender = (props: IAgiIntranetHomeMainProps) => {
+export const Calender = (props: any) => {
+    const WEEK_DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const [selectedDays, setSelectedDays] = useState(null);
+
     return (<div className="col-md-12 mt-4 ">
         <div className="card calendar rounded-0">
-
             <div className="card-body rounded-0">
-
                 <div className="app">
                     <div className="app__main">
                         <div className="calendar">
-                            <div id="calendar"></div>
-                            <div><span>Holiday</span></div>
+                            <DayPicker
+                                month={new Date()}
+                                selectedDays={selectedDays}
+                                weekdaysShort={WEEK_DAYS}
+                            />
+                            <div className="legend calendar-legend">
+                                <span>Today</span>
+                                <span>Holidays</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
         </div>
     </div>
