3f20478217142c4e0cb65614ceb78a45933f02b8
Mohammed Jabir <mohammed.jabir@techcarrot.ae>
2022-09-06 09:18:42 +0400

    rewards ui fix

--- AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/Rewards/Rewards.tsx
+++ AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/Rewards/Rewards.tsx
@@ -12,7 +12,7 @@ const RewardCarousel = (props: IReward) => {
     imageUrl = imageUrl?.serverUrl + imageUrl?.serverRelativeUrl;
     return (<>
         <div className={`carousel-item ${!props.index ? 'active' : ''}`}>
-            <img src={`${imageUrl}`} className="d-block w-100" alt="..." />
+            <img src={`${imageUrl}`} className="d-block w-100 rewards-image" alt="..." />
             <div className={`carousel-caption overlay ${styles.overlay}`}>
                 <p>{props.Description}</p>
                 <div className="offer-btn-container"><a href={`./Reward%20Details.aspx?rewardID=${props.Id}`} className="btn btn-lg btn-view-offer">View Offer</a></div>
@@ -46,33 +46,24 @@ export const Rewards = (props: IAgiIntranetHomeMainProps) => {
             <div data-bs-target="#employeeOffer" data-bs-toggle="collapse">
                 <div className="card-header d-flex align-items-center justify-content-between" >
                     <h4 className="card-title mb-0">Rewards</h4>
-                    <a href="./Rewards.aspx" className="viewall-link d-none d-md-block">View All</a>
-                    <div className="d-md-none " >
-                        <div className="float-right navbar-toggler d-md-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                                <g id="Dropdown-Logo" transform="translate(-84 -7.544)">
-                                    <path id="Path_73662" data-name="Path 73662" d="M15.739,7.87,8.525.656,7.868,0,0,7.87" transform="translate(100.366 20.883) rotate(180)" fill="none" stroke="#dccede" stroke-width="1.5" />
-                                    <rect id="Rectangle_7537" data-name="Rectangle 7537" width="18" height="18" transform="translate(84 7.544)" fill="none" />
-                                </g>
-                            </svg>
+                    <div className="d-flex align-items-center">
+                        <a href="./Rewards.aspx" className="viewall-link d-none d-md-block">View All</a>
+                        <div className="p-0 ms-3 position-relative d-none d-md-block">
+                            <button className="carousel-control-prev" type="button"
+                                data-bs-target="#employeeOffersControls"
+                                data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon"
+                                    aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button"
+                                data-bs-target="#employeeOffersControls"
+                                data-bs-slide="next">
+                                <span className="carousel-control-next-icon"
+                                    aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
                         </div>
-                    </div>
-                    <div className="p-0 position-relative d-none d-md-block">
-                        <button className="carousel-control-prev" type="button"
-                            data-bs-target="#employeeOffersControls"
-                            data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon"
-                                aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button"
-                            data-bs-target="#employeeOffersControls"
-                            data-bs-slide="next">
-                            <span className="carousel-control-next-icon"
-                                aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-
                     </div>
                 </div>
 
