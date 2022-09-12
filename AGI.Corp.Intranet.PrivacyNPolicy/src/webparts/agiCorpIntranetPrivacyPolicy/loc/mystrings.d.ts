fe53910c284459e6d058d1448df5b5d71d7265a6
Athira <athira.somanathan@techcarrot.ae>
2022-09-12 15:23:15 +0400

    PrivacyAnd policy first commitment

---
+++ AGI.Corp.Intranet.PrivacyNPolicy/src/webparts/agiCorpIntranetPrivacyPolicy/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAgiCorpIntranetPrivacyPolicyWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AgiCorpIntranetPrivacyPolicyWebPartStrings' {
+  const strings: IAgiCorpIntranetPrivacyPolicyWebPartStrings;
+  export = strings;
+}
