f6d912274187a1bc5420331e004b9270f665911f
Mohammed Jabir <mohammed.jabir@techcarrot.ae>
2022-09-13 16:42:26 +0400

    fix for search issue

--- AGI.Corp.Intranet.CompanyPolicy/src/webparts/agiIntranetCompanyPolicy/components/SearchBar/SearchBox.tsx
+++ AGI.Corp.Intranet.CompanyPolicy/src/webparts/agiIntranetCompanyPolicy/components/SearchBar/SearchBox.tsx
@@ -2,26 +2,23 @@ import * as React from 'react';
 import { useState } from 'react';
 
 export const SearchBox = (props: any) => {
-    const [searchField, setSearchField] = useState("");
-
-    const filteredPolicies = props.policies.filter(
+    const getFilteredPolicies = (keyword: string) => props.policies.filter(
         (policy: any) => {
             return (
                 policy
                     .Title
                     .toLowerCase()
-                    .includes(searchField.toLowerCase()) ||
+                    .includes(keyword.toLowerCase()) ||
                 policy
                     .PolicyDescription
                     .toLowerCase()
-                    .includes(searchField.toLowerCase())
+                    .includes(keyword.toLowerCase())
             );
         }
     );
 
     const handleChange = (e: any) => {
-        setSearchField(e.target.value);
-        props.showFilteredPolicies(filteredPolicies);
+        props.showFilteredPolicies(getFilteredPolicies(e.target.value));
     };
 
     return (
