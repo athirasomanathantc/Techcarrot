ccd54b8438d30a7196e4d59be5ef0e911bd0af65
Mohammed Jabir <mohammed.jabir@techcarrot.ae>
2022-09-15 16:16:54 +0400

    survey ui changes

--- AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/EmployeeSurvey/EmployeeSurvey.tsx
+++ AGI.Corp.Intranet.Home/src/webparts/agiIntranetHomeMain/components/EmployeeSurvey/EmployeeSurvey.tsx
@@ -140,74 +140,9 @@ export const EmployeeSurvey = (props: IAgiIntranetHomeMainProps) => {
                     <div className="card-body">
 
                         <div id="qbox-container">
-                            {currentQuestion.question && <form className="needs-validation" id="form-wrapper" method="post"
-                                name="form-wrapper">
-                                <div id="steps-container">
-                                    {!submitted && <div className="step d-block">
-                                        <h4>{currentQuestion.question.Title}</h4>
-                                        <div className="form-check ps-0 q-box">
-
-                                            {currentQuestion.options.map((option: ISurveyOption, index: number) => {
-                                                return (<>
-                                                    <div className="q-box__question">
-                                                        <input type="radio" id={`q_${index}`}
-                                                            name="survey-questions" checked={option.Checked} onChange={(e) => onChange(e, option)} />
-                                                        <label htmlFor={`q_${index}`}>{option.Title}</label>
-                                                    </div>
-                                                </>)
-                                            })}
-
-                                        </div>
-                                    </div>}
-
-                                    {submitted && <div id="success">
-                                        <div className="mt-5">
-                                            <h4>You have submitted your response!</h4>
-                                        </div>
-                                    </div>}
-
-                                </div>
-                                {!submitted && <div id="q-box__buttons">
-                                    <button id="prev-btn" type="button" className={currentQuestion.question.SortOrder === 1 ? 'd-none' : ''} onClick={() => movePrev(currentQuestion.question.SortOrder - 2)}>
-                                        <i><svg id="Group_8057" data-name="Group 8057"
-                                            xmlns="http://www.w3.org/2000/svg" width="30"
-                                            height="30" viewBox="0 0 30 30">
-                                            <g id="Ellipse_76" data-name="Ellipse 76"
-                                                fill="rgba(157,14,113,0.05)"
-                                                stroke="rgba(112,112,112,0.04)"
-                                                stroke-width="1">
-                                                <circle cx="15" cy="15" r="15" stroke="none" />
-                                                <circle cx="15" cy="15" r="14.5" fill="none" />
-                                            </g>
-                                            <path id="Path_73923" data-name="Path 73923"
-                                                d="M30.211,13.153a.56.56,0,1,1,.768.814l-4.605,4.35,4.605,4.35a.56.56,0,1,1-.768.814l-5.036-4.756a.56.56,0,0,1,0-.814l5.036-4.756Z"
-                                                transform="translate(-13.155 -3)"
-                                                fill="#9d0e71" />
-                                        </svg>
-                                        </i>
-                                        Previous
-
-                                    </button>
-                                    <button id="next-btn" type="button" className={questions.length === currentQuestion.question.SortOrder ? 'd-none' : 'd-inline-block'} onClick={() => moveNext(currentQuestion.question.SortOrder)}>Next
-                                        <i><svg id="Group_8056" data-name="Group 8056"
-                                            xmlns="http://www.w3.org/2000/svg" width="30"
-                                            height="30" viewBox="0 0 30 30">
-                                            <g id="Ellipse_76" data-name="Ellipse 76"
-                                                fill="rgba(157,14,113,0.05)"
-                                                stroke="rgba(112,112,112,0.04)"
-                                                stroke-width="1">
-                                                <circle cx="15" cy="15" r="15" stroke="none" />
-                                                <circle cx="15" cy="15" r="14.5" fill="none" />
-                                            </g>
-                                            <path id="Path_73923" data-name="Path 73923"
-                                                d="M25.944,13.153a.56.56,0,1,0-.768.814l4.605,4.35-4.605,4.35a.56.56,0,1,0,.768.814l5.036-4.756a.56.56,0,0,0,0-.814l-5.036-4.756Z"
-                                                transform="translate(-13 -3)" fill="#9d0e71" />
-                                        </svg></i>
-                                    </button>
-                                    <button id="submit-btn" type="submit"
-                                        className={questions.length === currentQuestion.question.SortOrder ? '' : 'd-none'} onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onSubmit(e)}>Submit</button>
-                                </div>}
-                            </form>}
+                            <img src={`${props.siteUrl}/assets/images/survey-icon.svg`}/>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+                            <a className="btn btn-lg btn-gradient">Start Survey</a>
                         </div>
 
 
