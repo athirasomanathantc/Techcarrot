a38525b843b5ce630c0abe896a0cb86c8d95521a
Athira <athira.somanathan@techcarrot.ae>
2022-10-17 09:58:51 +0400

    initial commit

---
+++ AGI.Corp.Intranet.Interim/src/webparts/agiCorpIntranemInterim/components/IAgiCorpIntranemInterimProps.ts
@@ -0,0 +1,9 @@
+export interface IAgiCorpIntranemInterimProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+  siteUrl:string;
+  context:any;
+}
---
+++ AGI.Corp.Intranet.Interim/src/webparts/agiCorpIntranemInterim/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAgiCorpIntranemInterimWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AgiCorpIntranemInterimWebPartStrings' {
+  const strings: IAgiCorpIntranemInterimWebPartStrings;
+  export = strings;
+}
