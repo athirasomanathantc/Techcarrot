e9c69a1f9725fc689fea938e09b4b87d921e2ca2
Gokul <gokula.sivaraja@techcarrot.ae>
2022-10-25 12:36:01 +0400

    library navigation

---
+++ AGI.Corp.Intranet.Gallery.Library.Script/src/webparts/agiIntranetGalleryLibraryScript/AgiIntranetGalleryLibraryScriptWebPart.ts
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+
+import * as strings from 'AgiIntranetGalleryLibraryScriptWebPartStrings';
+import AgiIntranetGalleryLibraryScript from './components/AgiIntranetGalleryLibraryScript';
+import { IAgiIntranetGalleryLibraryScriptProps } from './components/IAgiIntranetGalleryLibraryScriptProps';
+
+export interface IAgiIntranetGalleryLibraryScriptWebPartProps {
+  description: string;
+}
+
+export default class AgiIntranetGalleryLibraryScriptWebPart extends BaseClientSideWebPart<IAgiIntranetGalleryLibraryScriptWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  public render(): void {
+    const element: React.ReactElement<IAgiIntranetGalleryLibraryScriptProps> = React.createElement(
+      AgiIntranetGalleryLibraryScript,
+      {
+        description: this.properties.description,
+        isDarkTheme: this._isDarkTheme,
+        environmentMessage: this._environmentMessage,
+        hasTeamsContext: !!this.context.sdks.microsoftTeams,
+        userDisplayName: this.context.pageContext.user.displayName,
+
+        siteUrl: this.context.pageContext.web.absoluteUrl
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onInit(): Promise<void> {
+    this._environmentMessage = this._getEnvironmentMessage();
+
+    const checkElement = async (selector: any) => {
+      while (document.querySelector(selector) === null) {
+        await new Promise(resolve => requestAnimationFrame(resolve))
+      }
+      return document.querySelector(selector);
+    };
+
+    checkElement('[data-viewport-id^="WebPart.ListWebPart"]').then((selector) => {
+      selector.classList.add('container');
+      selector.classList.add('listWebpartContainer');
+    });
+
+    return super.onInit();
+  }
+
+  private _getEnvironmentMessage(): string {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams
+      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    }
+
+    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ AGI.Corp.Intranet.Gallery.Library.Script/src/webparts/agiIntranetGalleryLibraryScript/components/IAgiIntranetGalleryLibraryScriptProps.ts
@@ -0,0 +1,9 @@
+export interface IAgiIntranetGalleryLibraryScriptProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+
+  siteUrl: string;
+}
