ea140d6242885661292283d7466809d3ed26b624
Nirali Thakkar <nirali.thakkar@techcarrot.ae>
2022-11-02 11:48:33 +0530

    made blogs title dynamic

--- AGI.Corp.Intranet.Blogs/src/webparts/agiCorpIntranetBlogs/components/AgiCorpIntranetBlogs.tsx
+++ AGI.Corp.Intranet.Blogs/src/webparts/agiCorpIntranetBlogs/components/AgiCorpIntranetBlogs.tsx
@@ -248,12 +248,11 @@ export default class AgiCorpIntranetBlogs extends React.Component<IAgiCorpIntran
     });
   }
   private async getTitle(): Promise<void> {
-    debugger;
-    sp.web.lists.getByTitle(LIST_BLOG_TITLE).items.get().then((items: IBlogTitle[]) => {
+    sp.web.lists.getByTitle('TitleConfig').items.filter("Title eq 'Blogs Title'")
+    .get().then((items: any) => {
       this.setState({
-        blogTitle: items[8]?.Header,
+        blogTitle: items[0]?.Header
       });
-      console.log(items[8]?.Header);
     });
   }
   private scrollToTop(): void {
